81eb55d2cc39c8b62bfd968c865028885b603634
jshen130 <jshen130@gmail.com>
2016-03-16 00:49:18 -0700

    test CI

--- GruntFile.js
+++ GruntFile.js
@@ -1,46 +1,46 @@
 module.exports = function(grunt){
-	grunt.initConfig({
-      pkg:grunt.file.readJSON('package.json'),
-      mochaTest:{
-      	local:{
+    grunt.initConfig({
+	pkg:grunt.file.readJSON('package.json'),
+	mochaTest:{
+      	    local:{
       		options:{
-                 reporter:'spec',
-                 quiet:false,
-                 clearRequireCache:false,
-                 ui:'tdd'
+                    reporter:'spec',
+                    quiet:false,
+                    clearRequireCache:false,
+                    ui:'tdd'
       		},
       		src:['test/**/*.js']
-      	},
-      	shippable:{
+      	    },
+      	    shippable:{
       		options:{
-      			reporter:'mocha-junit-reporter',
+      		    reporter:'mocha-junit-reporter',
       			reportOptions:{
-      				mochaFile:'shippable/testresults/results.xml'
+      			    mochaFile:'shippable/testresults/results.xml'
       			},
-      			ui:'tdd'
+      		    ui:'tdd'
       		},
       		src:['test/**/*.js']
-      	}
+      	    }
       },
-      mocha_istanbul:{
-      	coverage:{
+	mocha_istanbul:{
+      	    coverage:{
       		scr:'test',
       		options:{
-      			mocha_Options:['--ui','tdd']
+      		    mocha_Options:['--ui','tdd']
       		}
-      	}
-      }
-	});
-  
-	grunt.loadNpmTasks('grunt-mocha-test');
-	grunt.loadNpmTasks('grunt-mocha-istanbul');
+      	    }
+	}
+    });
+    
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-mocha-istanbul');
+    
+    grunt.registerTask('default', []);
+    
+    grunt.registerTask('test',['mochaTest:local']);
 
-	grunt.registerTask('default', []);
-  
-	grunt.registerTask('test',['mochaTest:local']);
-
-	grunt.registerTask('shippable',['mochaTest:shippable','mocha_istanbul']);
-
-	grunt.registerTask('coverage',['mocha_istanbul']);
-
+    grunt.registerTask('shippable',['mochaTest:shippable','mocha_istanbul']);
+    
+    grunt.registerTask('coverage',['mocha_istanbul']);
+    
 };

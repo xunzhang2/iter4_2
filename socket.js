65c22ac79d64ab6456c6d537fd0d56b3408203d1
MellieWu <MellieWu@users.noreply.github.com>
2016-03-17 09:32:25 -0700

    collect user socket.id

--- socket.js
+++ socket.js
@@ -1,17 +1,36 @@
-module.exports = function(io, db) {
-var socketQueue = [];
+var HashMap = require('hashmap');
 
+module.exports = function(io, db) {
+  var map = new HashMap();
+  
   io.on("connection", function(socket){
-    
+//============================ store clients =====================================
+    socket.on('usersList', function (data) {
+      if(map.has(data.name)){
+        var list = JSON.parse(map.get(data.name));
+        list.push(socket.id);
+        map.set(data.name,JSON.stringify(list));
+      }else{
+        var newlist = [];
+        newlist.push(socket.id);
+        map.set(data.name, JSON.stringify(newlist));
+
+      }
+       
+       console.log("online users "+map.keys()); 
+
+    });
+//============================ send back online user list =====================================
+    socket.on('usersListReq', function(){
+         socket.emit('usersListRes', map.keys());
+    });  
 //============================ PUBLIC CHAT =====================================
     // reveive new public message from user, send it to other users
     socket.on('sendPublicMessage', function(data){
       io.emit("broadcastPublicMessage", data);
       db.saveMessages(data.message, current_time(), data.name, function(){});
     });
-//============================ PUBLIC CHAT =====================================
 
-    console.log('a user connected');
 
 //============================ ANNOUNCEMENT =====================================
  // reveive announcements from user
